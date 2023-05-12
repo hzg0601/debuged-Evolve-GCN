c32929899440dae5b920f419451115a5b5e027e9
hzg0601 <hzg0601@163.com>
2023-05-12 13:49:31 +0800

    modify bash_python_ml

--- bash_python_ml.sh
+++ bash_python_ml.sh
@@ -6,12 +6,18 @@ file='./experiments/parameters_example.yaml'
 i=0
 for model in ${models[@]};
 do  
-    echo currently processing model, feature,ne is: $model,${features[i]},${nes[i]}
-    sed -i "90s/^model:.*$/model: ${model}/g" $file
-    sed -i "91s/^feature:.*$/feature: ${features[i]}/g" $file
-    sed -i "92s/^ne:.*$/ne: ${nes[i]}/g" $file
-    nohup python ml_models.py --config_file $file > "./log/log3_${model}_${features[i]}_${nes[i]}.log" 2>&1 &
-    echo the id of current process is: $!
-    wait $!
-    let i++
+    for feature in ${features[@]};
+    do
+        for ne in ${nes[@]};
+        do
+        echo currently processing model, feature,ne is: $model,${feature},${ne}
+        sed -i "90s/^model:.*$/model: ${model}/g" $file
+        sed -i "91s/^feature:.*$/feature: ${feature}/g" $file
+        sed -i "92s/^ne:.*$/ne: ${ne}/g" $file
+        nohup python ml_models.py --config_file $file > "./log/log3_${model}_${feature}_${ne}.log" 2>&1 &
+        echo the id of current process is: $!
+        wait $!
+        let i++
+        done
+    done
 done
