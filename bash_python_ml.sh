3e2188aec33c7b8204564dfa07448d14d7fafeb3
Zhi-guo Huang <hzg0601@163.com>
2023-05-16 11:12:43 +0800

    Update bash_python_ml.sh

--- bash_python_ml.sh
+++ bash_python_ml.sh
@@ -10,14 +10,21 @@ do
     do
         for ne in ${nes[@]};
         do
-            echo currently processing model, feature,ne is: $model,${feature},${ne}
-            sed -i "90s/\smodel: .*$/\tmodel: ${model}/g" $file
-            sed -i "91s/\sfeature: .*$/\tfeature: ${feature}/g" $file
-            sed -i "92s/\sne: .*$/\tne: ${ne}/g" $file
-            nohup python ml_models.py --config_file $file > "./log/log3_${model}_${feature}_${ne}.log" 2>&1 &
+            
+
+            echo currently processing model, feature,ne is: ${model},${feature},${ne}
+            sed -i "91s/[\t|(  )]model: .*$/ model: ${model}/g" $file
+            sed -i "93s/[\t|(  )]feature: .*$/ feature: ${feature}/g" $file
+            sed -i "95s/[\t|(  )]ne: .*$/ ne: ${ne}/g" $file
+            nohup python ml_models.py --config_file $file >> ./log/log3_ml.log 2>&1 & #"./log/log3_${model}_${feature}_${ne}.log" 2>&1 &
             echo the id of current process is: $!
             wait $!
             let i++
+            # 字符串 用== 数字 -eq
+            if ( [ $feature == AF ] || [ $feature == LF ] );then
+                echo jump out of current loop of nes when feature equals to AF or LF.
+                continue 2
+            fi
         done
     done
 done
